a1d15abf16171ecba9ef87452cd067c2f0f783fa
韩西瓜 <sohanyu@126.com>
2023-06-29 20:12:11 +0800

    feat:重构&加评论

--- config.js
+++ config.js
@@ -32,6 +32,42 @@ var infos = [
     "site": "https://vlieo.com",
     "avatar": "https://vlieo.com/images/avatar.png?v=1687790747714",
     "comment":true
+  },{
+    "name": "veryjack",
+    "api": "https://say.veryjack.com",
+    "site": "https://veryjack.com",
+    "avatar": "https://pix.veryjack.com/i/2023/04/04/fsxnkv.webp",
+    "comment":true
+  },{
+    "name": "eirms",
+    "api": "https://memo.eirms.com",
+    "site": "https://eirms.com",
+    "avatar": "https://dogefs.s3.ladydaily.com/lucy/storage/1680832936501.png",
+    "comment":true
+  },{
+    "name": "TeacherDu平台",
+    "api": "https://s.dusays.com",
+    "site": "https://dusays.com",
+    "avatar": "https://cravatar.cn/avatar/c1b204bab687a23c8b6d7c8de11c7c59",
+    "comment":true
+  },{
+    "name": "l22",
+    "api": "https://memos.l22.org",
+    "site": "https://l22.org",
+    "avatar": "https://cravatar.cn/avatar/4b0d33a08ac73dc07a5293f14232ca53",
+    "comment":true
+  },{
+    "name": "isay",
+    "api": "https://isay.live",
+    "site": "https://xsinger.me",
+    "avatar": "https://cravatar.cn/avatar/f0e33f4d097fe2e9fd74b9b129e7a655",
+    "comment":true
+  },{
+    "name": "印记",
+    "api": "https://yinji.net",
+    "site": "https://yinji.net",
+    "avatar": "https://sdn.geekzu.org/avatar/ba98152d337d7be289b51b90769cb194",
+    "comment":true
   }
 ];
 
